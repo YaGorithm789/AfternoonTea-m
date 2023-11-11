880f25ce07dd42c383bd42b766b9dd905d8f3b21
Daehoon Lee <dleognsdl1@naver.com>
2023-11-12 00:08:52 +0900

    공 바꾸기

---
+++ hoon/10813.swift
@@ -0,0 +1,12 @@
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m) = (input[0], input[1])
+var box = [Int](repeating: 0, count: n)
+(0..<n).forEach { box[$0] = $0 + 1 }
+
+for _ in 0..<m {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let (i, j) = (input[0] - 1, input[1] - 1)
+    box.swapAt(i, j)
+}
+
+box.forEach { print($0, terminator: " ") }
