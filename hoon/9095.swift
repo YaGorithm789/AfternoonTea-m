6c871e48e69c38af2fd845f91edf1afe6e61d99d
Daehoon Lee <dleognsdl1@naver.com>
2024-02-10 22:12:07 +0900

    1, 2, 3 더하기

---
+++ hoon/9095.swift
@@ -0,0 +1,14 @@
+let t = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 11)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in 4..<11 {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+}
+
+for _ in 1...t {
+    let n = Int(readLine()!)!
+    print(dp[n])
+}
