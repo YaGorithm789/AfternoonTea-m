a2832a17a419ac72b421bee39c4040563bb6a15a
Daehoon Lee <dleognsdl1@naver.com>
2024-07-21 23:38:21 +0900

    괄호의 값

---
+++ hoon/2504.swift
@@ -0,0 +1,45 @@
+var parens = Array(readLine()!).map { String($0) }
+var stack = [String]()
+var result = 0
+var isCorrect = true
+var temp = 1
+
+for i in parens.indices {
+    if parens[i] == "(" {
+        temp *= 2
+        stack.append(parens[i])
+    } else if parens[i] == "[" {
+        temp *= 3
+        stack.append(parens[i])
+    } else if parens[i] == ")" {
+        if stack.isEmpty || stack.last != "(" {
+            isCorrect = false
+            break
+        }
+        
+        if parens[i-1] == "(" {
+            result += temp
+        }
+      
+        stack.removeLast()
+        temp /= 2
+    } else if parens[i] == "]" {
+        if stack.isEmpty || stack.last != "[" {
+            isCorrect = false
+            break
+        }
+        
+        if parens[i-1] == "[" {
+            result += temp
+        }
+      
+        stack.removeLast()
+        temp /= 3
+    }
+}
+
+if !isCorrect || !stack.isEmpty {
+    print(0)
+} else {
+    print(result)
+}
