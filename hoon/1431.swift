1a59354cdd88c536a21e42da37bec1b962bb9dff
Daehoon Lee <dleognsdl1@naver.com>
2024-09-18 23:34:23 +0900

    시리얼 번호

---
+++ hoon/1431.swift
@@ -0,0 +1,32 @@
+var serials = [String]()
+
+for _ in 0 ..< Int(readLine()!)! {
+    serials.append(readLine()!)
+}
+
+func getSum(_ s: String) -> Int {
+    let str = Array(s).map { String($0) }
+    var sum = 0
+    
+    for char in str {
+        if Int(char) != nil {
+            sum += Int(char)!
+        }
+    }
+    
+    return sum
+}
+
+serials.sort {
+    if $0.count == $1.count {
+        if getSum($0) == getSum($1) {
+            return $0 < $1
+        } else {
+            return getSum($0) < getSum($1)
+        }
+    } else {
+        return $0.count < $1.count
+    }
+}
+
+serials.forEach { print($0) }
