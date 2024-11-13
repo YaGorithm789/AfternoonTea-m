4e603f0742234bebaecff097e3bd1427c521cc56
Daehoon Lee <dleognsdl1@naver.com>
2024-11-13 23:52:08 +0900

    히스토그램에서 가장 큰 직사각형

--- hoon/6549.swift
+++ hoon/6549.swift
@@ -1,22 +1,52 @@
 while true {
     let line = readLine()!.split(separator: " ").map { Int($0)! }
     let n = line[0]
+    let arr = Array(line[1...])
     
     if n == 0 { break }
     
-    let arr = Array(line[1...]) + [-1]
-    var maxArea = 0
-    var stack = [-1]
-    
-    for i in 0...n {
-        while stack.count > 1 && arr[stack.last!] > arr[i] {
-            let height = arr[stack.removeLast()]
-            let width = i - stack.last! - 1
-            maxArea = max(maxArea, height * width)
+    func maxArea(_ left: Int, _ right: Int) -> Int {
+        if left >= right {
+            return arr[right]
         }
         
-        stack.append(i)
+        let mid = (left + right) / 2
+        
+        return max(maxArea(left, mid), maxArea(mid + 1, right), centerMaxArea(left, right))
     }
     
-    print(maxArea)
+    func centerMaxArea(_ left: Int, _ right: Int) -> Int {
+        let mid = (left + right) / 2
+        var (start, end) = (mid, mid)
+        var height = arr[start]
+        var area = height
+        
+        while left < start && end < right {
+            if arr[start - 1] > arr[end + 1] {
+                start -= 1
+                height = min(height, arr[start])
+            } else {
+                end += 1
+                height = min(height, arr[end])
+            }
+            
+            area = max(area, height * (end - start + 1))
+        }
+        
+        while left < start {
+            start -= 1
+            height = min(height, arr[start])
+            area = max(area, height * (end - start + 1))
+        }
+        
+        while end < right {
+            end += 1
+            height = min(height, arr[end])
+            area = max(area, height * (end - start + 1))
+        }
+        
+        return area
+    }
+    
+    print(maxArea(0, n - 1))
 }
