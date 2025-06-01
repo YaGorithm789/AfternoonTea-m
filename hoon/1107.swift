2ab21791cd7bb636a04985bd73611dfef3252156
Daehoon Lee <dleognsdl1@naver.com>
2025-06-01 22:19:14 +0900

    리모컨

---
+++ hoon/1107.swift
@@ -0,0 +1,27 @@
+let strN = readLine()!
+let n = Int(strN)!
+var failures = Set<Int>()
+var minOrder = abs(n - 100)
+
+if Int(readLine()!)! != 0 {
+    failures = Set<Int>(readLine()!.split(separator: " ").map { Int($0)! })
+}
+
+func makeAllNumbers(num: String) {
+    if num.count >= 6 {
+        return
+    }
+    
+    for i in 0..<10 {
+        if failures.contains(i) {
+            continue
+        } else {
+            let orderCount = abs(n - Int(num + String(i))!) + num.count + 1
+            minOrder = min(minOrder, orderCount)
+            makeAllNumbers(num: num + String(i))
+        }
+    }
+}
+
+makeAllNumbers(num: "")
+print(minOrder)
