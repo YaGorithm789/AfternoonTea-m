87d218e6a6eae631f9b8391aa26d42ecf617a73c
Daehoon Lee <dleognsdl1@naver.com>
2025-06-20 00:27:18 +0900

    폴리오미노

--- hoon/1343.swift
+++ hoon/1343.swift
@@ -1,27 +1,6 @@
-func cover(_ str: String) -> String {
-    let n = str.count
-    
-    if n % 2 == 1 {
-        return str
-    }
-    
-    return String(repeating: "AAAA", count: n / 4) + String(repeating: "BB", count: (n % 4) / 2)
-}
+import Foundation
 
-let board = readLine()!
-var x = ""
-var answer = ""
-
-for b in board {
-    if b == "." {
-        answer += cover(x)
-        answer += "."
-        x = ""
-        continue
-    }
-    
-    x += String(b)
-}
-
-answer += cover(x)
-answer.contains("X") ? print(-1) : print(answer)
+var board = readLine()!
+board = board.replacingOccurrences(of: "XXXX", with: "AAAA")
+board = board.replacingOccurrences(of: "XX", with: "BB")
+board.contains("X") ? print(-1) : print(board)
