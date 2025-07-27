d04b3b588eaba15e12dea34b153f5101e409025e
Daehoon Lee <dleognsdl1@naver.com>
2025-07-27 23:44:33 +0900

    Contact

--- hoon/1013.swift
+++ hoon/1013.swift
@@ -1,51 +1,18 @@
-let n = Int(readLine()!)!
+import Foundation
 
-func isValidPattern(_ pattern: String) -> Bool {
-    // (100+1+ | 01)+
-    var state = 0
+func solve() {
+    let t = Int(readLine()!)!
     
-    for char in pattern {
-        if let nextState = next(flag: char, from: state) {
-            state = nextState
+    for _ in 0..<t {
+        let input = readLine()!
+        let regex = try! NSRegularExpression(pattern: "^(100+1+|01)+$", options: [])
+        
+        if regex.firstMatch(in: input, options: [], range: NSRange(location: 0, length: input.utf16.count)) != nil {
+            print("YES")
         } else {
-            return false
-        }
-    }
-    
-    return [0, 2, 6, 7].contains(state)
-}
-
-func next(flag: Character, from state: Int) -> Int? {
-    if flag == "0" {
-        switch state {
-            case 0: return 1
-            case 1: return nil
-            case 2: return 1
-            case 3: return 4
-            case 4: return 5
-            case 5: return 5
-            case 6: return 1
-            case 7: return 8
-            case 8: return 5
-            default: return nil
-        }
-    } else {
-        switch state {
-            case 0: return 3
-            case 1: return 2
-            case 2: return 3
-            case 3: return nil
-            case 4: return nil
-            case 5: return 6
-            case 6: return 7
-            case 7: return 7
-            case 8: return 0
-            default: return nil
+            print("NO")
         }
     }
 }
 
-for _ in 0..<n {
-    let pattern = readLine()!
-    print(isValidPattern(pattern) ? "YES" : "NO")
-}
+solve()
