59eaf7fbc8ef5c1bcd9c8e352250930509e55a90
Daehoon Lee <dleognsdl1@naver.com>
2025-10-25 00:10:43 +0900

    학생 번호
    Fix logic for reading input and counting unique prefixes.

--- hoon/1235.swift
+++ hoon/1235.swift
@@ -2,19 +2,19 @@ let N = Int(readLine()!)!
 var sn = [String](repeating: "", count: N)
 
 for i in 0..<N {
-    sn[i] = String(readLine()!.reversed())
+    sn[i] = readLine()!
 }
 
-for i in 0..<sn[0].count {
+for i in 0...sn[0].count {
     var set = Set<String>()
-    let end = sn[0].index(sn[0].startIndex, offsetBy: i)
+    let start = sn[0].index(sn[0].endIndex, offsetBy: -i)
     
     for j in 0..<N {
-        set.insert(String(sn[j][...end]))
+        set.insert(String(sn[j][start...]))
     }
     
     if set.count == N {
-        print(i + 1)
+        print(i)
         break
     }
 }
